6f89ad65b64836b95b7b6b4c47d1a93afe57b895
imbasri <imbasri.dev@gmail.com>
2023-02-15 22:44:56 +0700

    done

--- src/router.jsx
+++ src/router.jsx
@@ -3,16 +3,16 @@ import App from "./App";
 import ErrorPage from "./components/error/Error";
 const router = createBrowserRouter([
    {
-      path: `*`,
-      element: <ErrorPage />,
-   },
-   {
       path: `/`,
       element: <App />,
    },
    {
       path: `/search/*`,
       element: <App />,
+   },
+   {
+      path: `*`,
+      element: <ErrorPage />,
    },
 ]);
 export default router;
